2f9f26b28c673f4d4b11f49b6897c6b9913ccc9e
fations <93991735+fations@users.noreply.github.com>
2022-02-06 10:53:21 -0500

    Add files via upload

---
+++ hello_world/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut x = 10.32156566;
+    println!("x is {}",x);
+    x = 20.0;
+    println!("x is {}",x);
+}
---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\fatio\Documents\vs code\rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\fatio\Documents\vs code\rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
